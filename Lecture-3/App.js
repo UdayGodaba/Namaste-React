4fc4947329fd2817a2c4152bc2542c79e978abe2
UdayGodaba <godabauday@gmail.com>
2024-01-17 22:50:13 +0530

    L3 done with JSX and functional components understanding

--- Lecture-3/App.js
+++ Lecture-3/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Elements
+
 const heading = React.createElement(
   "h1",
   {
@@ -9,5 +11,24 @@ const heading = React.createElement(
   "Hello World from React"
 );
 
+console.log(heading);
+const jsxHeading = <h1>Hello from JSX</h1>;
+console.log(jsxHeading);
+
+// React components
+
+const Component1 = () => {
+  return (
+    <>
+      <Component2 />
+      <h1>Hello from Component1</h1>
+    </>
+  );
+};
+
+const Component2 = () => <h1>Hello from Component2</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+// root.render(jsxHeading);
+root.render(<Component1 />);
