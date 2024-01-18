8485619a47a65ae1a2b5927ac26bfbedda12afad
UdayGodaba <godabauday@gmail.com>
2024-01-18 18:07:14 +0530

    L-4 done with props, functional component reusability

--- Lecture-4/App.js
+++ Lecture-4/App.js
@@ -1,5 +1,22 @@
 import ReactDOM from "react-dom";
 
+/*
+  Header
+  - Logo
+  - Nav Items
+  Body
+  - Search
+  - RestaurantConatiner
+    - RestaurantCard
+      - Img
+      - Name of Res, Star Rating
+  Footer
+  - Copyright
+  - Links
+  - Address
+  - Contact
+*/
+
 const Header = () => {
   return (
     <div className="header">
@@ -21,10 +38,46 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.rating} stars</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          resName="Meghana Foods"
+          cuisine="Biryani, North Indian, Asian"
+          rating="4.4"
+        />
+        <RestaurantCard
+          resName="KFC"
+          cuisine="Burger, Fries, Fried Chicken"
+          rating="4.8"
+        />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 };
